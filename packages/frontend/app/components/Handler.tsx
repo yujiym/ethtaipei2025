9b1ed89fd383ce8790cb6a8aab27f88f8e6aabb4
Yuji Yamaguchi <yuji.ym@gmail.com>
2025-04-06 08:54:40 +0800

    update redirect

--- packages/frontend/app/components/Handler.tsx
+++ packages/frontend/app/components/Handler.tsx
@@ -1,29 +1,35 @@
-import { useEffect } from 'react'
-import { useAccount } from 'wagmi'
-import type { RootLoader } from '~/root'
+import { SpinnerGap } from "@phosphor-icons/react";
+import { useEffect } from "react";
+import { useAccount } from "wagmi";
+import type { RootLoader } from "~/root";
 
 function Loading() {
   return (
     <div className="fixed top-0 right-0 bottom-0 left-0 z-50 grid place-items-center bg-gray-500/60 backdrop-blur">
-      <ph-spinner-gap weight="bold" size="80" className="animate-spin-slow" />
+      <SpinnerGap weight="bold" size="80" className="animate-spin-slow" />
     </div>
-  )
+  );
 }
 
 export default function AppHandler({
   ld,
   navigate,
   pathname,
-  isNavigating
-}: { ld: RootLoader; navigate: (path: string) => void; pathname: string; isNavigating: boolean }) {
-  const { isConnected, isConnecting } = useAccount()
+  isNavigating,
+}: {
+  ld: RootLoader;
+  navigate: (path: string) => void;
+  pathname: string;
+  isNavigating: boolean;
+}) {
+  const { isConnected, isConnecting } = useAccount();
 
   // redirect
   useEffect(() => {
-    if (pathname !== '/' && !isConnected) {
-      navigate('/')
+    if (pathname !== "/" && !isConnected) {
+      // navigate("/");
     }
-  }, [isConnected, navigate, pathname])
+  }, [isConnected, navigate, pathname]);
 
-  return <>{!ld || isNavigating || (isConnecting && <Loading />)}</>
+  return <>{!ld || isNavigating || (isConnecting && <Loading />)}</>;
 }
